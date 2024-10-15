1b3b3a84f2ad29391c15d02168c8d9094a6a44fa
bluesea-developer <80672777@qq.com>
2024-10-15 20:41:02 +0800

    Add files via upload

---
+++ ColorWindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace ColorWindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(43, 10);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(64, 18);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "起始颜色";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(147, 10);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(26, 24);
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(201, 13);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(64, 18);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "终止颜色";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(304, 10);
+            this.pictureBox3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(26, 24);
+            this.pictureBox3.TabIndex = 4;
+            this.pictureBox3.TabStop = false;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(374, 13);
+            this.button3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(64, 18);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "显示";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(0, 39);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1629, 800);
+            this.panel1.TabIndex = 6;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1543, 4000);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1649, 572);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ ColorWindowsFormsApplication1/Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Threading;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ColorWindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            //using (LinearGradientBrush brush = new LinearGradientBrush(
+            //this.ClientRectangle,
+            //pictureBox2.BackColor,
+            //pictureBox3.BackColor,
+            //LinearGradientMode.Horizontal))
+            //{
+            //    // 填充渐变到窗体的客户区
+            //    e.Graphics.FillRectangle(brush, this.ClientRectangle);
+            //}
+
+            //base.OnPaint(e);
+
+            float w = 20f;
+            float h = 20f;
+
+            float zoom = 1.0f;
+
+            RectangleF rect1 = new RectangleF(0, 0, w*zoom, h* zoom);
+
+            Color c1 = Color.FromArgb(255, 128, 128);
+            Color c2 = Color.FromArgb(255, 255, 128);
+            Color c3 = Color.FromArgb(128, 255, 128);
+            Color c4 = Color.FromArgb(0, 255, 128);
+            Color c5 = Color.FromArgb(128, 255, 255);
+            Color c6 = Color.FromArgb(0, 128, 255);
+            Color c7 = Color.FromArgb(255, 128, 192);
+            Color c8 = Color.FromArgb(255, 128, 255);
+            Color c9 = Color.FromArgb(255, 0, 0);
+            Color c10 = Color.FromArgb(255, 255, 0);
+
+            List<Color> cList = new List<Color>();
+
+            cList.Add(c1);
+            cList.Add(c2);
+            cList.Add(c3);
+            cList.Add(c4);
+            cList.Add(c5);
+            cList.Add(c6);
+            cList.Add(c7);
+            cList.Add(c8);
+            cList.Add(c9);
+            cList.Add(c10);
+
+            Random random = new Random();
+
+            for (int i = 0; i < 200; i++)
+            {
+                for (int j = 0; j < 50; j++)
+                {
+                    RectangleF tempRect = new RectangleF(j* w*zoom, i* h * zoom, w*zoom, h * zoom);
+
+                    int randomIndex = random.Next(cList.Count);
+
+                    Color tc1 = cList[randomIndex];
+
+                    //Thread.Sleep(200);
+
+                    randomIndex = random.Next(cList.Count);
+
+                    Color tc2 = cList[randomIndex];
+
+
+                    LinearGradientBrush brush = new LinearGradientBrush(
+                    tempRect,
+                    tc1,
+                    tc2,
+                    LinearGradientMode.Horizontal);
+
+                    e.Graphics.FillRectangle(brush, tempRect);
+                }
+            }
+
+
+            
+
+
+            base.OnPaint(e);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult result = colorDialog1.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                // 获取用户所选颜色
+                //Color selectedColor = colorDialog1.Color;
+
+                // 在 label1 中显示所选颜色的 RGB 值
+                pictureBox2.BackColor = colorDialog1.Color;
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DialogResult result = colorDialog1.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                // 获取用户所选颜色
+                //Color selectedColor = colorDialog1.Color;
+
+                // 在 label1 中显示所选颜色的 RGB 值
+                pictureBox3.BackColor = colorDialog1.Color;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Refresh();
+        }
+    }
+}
